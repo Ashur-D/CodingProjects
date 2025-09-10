2eced3a6cd4edf2330dc6318825c37caa5b1c71d
ashur <ashur25batto@gmail.com>
2025-09-10 13:49:45 -0400

    changes

---
+++ PYTHON/week3/prog.py
@@ -0,0 +1,10 @@
+# working with variables and numbers
+
+# in python variables can take intgers (integers cannot have decimals) or floats (floats can have decimals)
+
+value1 = 139 # integer that takes 4 bytes in RAM
+value2 = -26 # 
+sum1 = value1 + value2 #sum1 is a variable
+print("The sum of", value1, "and", value2, "is", sum1)
+
+
