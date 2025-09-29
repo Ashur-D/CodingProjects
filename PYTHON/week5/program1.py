339bc947a51913c99c82b35f161718557b4d4e2d
Ashur-D <ashur25batto@gmail.com>
2025-09-29 14:45:30 -0400

    changes

---
+++ PYTHON/week5/program1.py
@@ -0,0 +1,8 @@
+first_name = "ashur"
+last_name = "dawod"
+full_name = f"{first_name}, {last_name}"
+print(full_name)
+print(f"{first_name}, {last_name}") # f string can be used in the print statemnt
+
+
+
