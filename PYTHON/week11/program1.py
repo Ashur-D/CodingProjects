29802a194e237cae3afbcd781f1106466dc250ff
Ashur-D <ashur25batto@gmail.com>
2025-11-18 16:22:32 -0500

    update

---
+++ PYTHON/week11/program1.py
@@ -0,0 +1,20 @@
+# a program that calculates the value of mpg using a user defiened function named, calcMPG
+
+def calcMPG():
+    miles, gallons = input('Enter miles and gallons: ').split()
+    miles, gallons = int(miles), int(gallons)
+
+    mpg = miles/gallons
+
+    return mpg # reutnring the value of mpg to the calling statement
+
+# calling the function to be executed by the interpreter, it execs line 12 first.
+# interpeter jumps from 13 to line 3
+# line 9 is the last line to exec
+
+mpg = calcMPG() # python looks for the calling statement first
+print('mpg:',round(mpg,2))
+# print('mpg: ' + str(round(mpg,2)))
+
+print('mpg ' + str(round(calcMPG(),2)))
+# print('mpg ' , (round(calcMPG(),2)))
