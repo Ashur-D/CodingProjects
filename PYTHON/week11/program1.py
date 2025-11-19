f693c1d01024fb7ae77244f81b7ab91b85f18d4b
Ashur-D <ashur25batto@gmail.com>
2025-11-19 17:17:54 -0500

    updates

--- PYTHON/week11/program1.py
+++ PYTHON/week11/program1.py
@@ -6,15 +6,15 @@ def calcMPG():
 
     mpg = miles/gallons
 
-    return mpg # reutnring the value of mpg to the calling statement
+    return mpg # returning the value of mpg to the calling statement
 
 # calling the function to be executed by the interpreter, it execs line 12 first.
 # interpeter jumps from 13 to line 3
 # line 9 is the last line to exec
 
 mpg = calcMPG() # python looks for the calling statement first
-print('mpg:',round(mpg,2))
+print('mpg: ',round(mpg,2))
 # print('mpg: ' + str(round(mpg,2)))
 
-print('mpg ' + str(round(calcMPG(),2)))
+print('mpg: ' + str(round(calcMPG(),2)))
 # print('mpg ' , (round(calcMPG(),2)))
