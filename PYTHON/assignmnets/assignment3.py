5eb714dd1f1a7ac735dc056043dfdd4abfa6c0b6
Ashur <ashurdawod@proton.me>
2025-12-03 19:34:18 -0500

    updates

--- PYTHON/assignmnets/assignment3.py
+++ PYTHON/assignmnets/assignment3.py
@@ -16,56 +16,56 @@
 • Make sure to ask the user if they want to run the application again.
 '''
 
-def calcDiscounts(customerType,totalValue):
-    discount = 0.0
-    if customerType == 'R':
-        if totalValue >= 250:
-            discount = 0.25
-        elif totalValue >= 100 and totalValue < 250:
-            discount = 0.10
-        else:
-            discount = 0.05
-    elif customerType == 'C':
-        if totalValue >= 250:
-            discount = 0.30
-        elif totalValue < 250:
-            discount = 0.20
-    else:
-        discount = 0
-    return discount
+# def calcDiscounts(customerType,totalValue):
+#     discount = 0.0
+#     if customerType == 'R':
+#         if totalValue >= 250:
+#             discount = 0.25
+#         elif totalValue >= 100 and totalValue < 250:
+#             discount = 0.10
+#         else:
+#             discount = 0.05
+#     elif customerType == 'C':
+#         if totalValue >= 250:
+#             discount = 0.30
+#         elif totalValue < 250:
+#             discount = 0.20
+#     else:
+#         discount = 0
+#     return discount
 
-def main():
-    repeat = 'YES'
+# def main():
+#     repeat = 'YES'
 
-    while repeat:
-        count = 0
-        user = int(input('Enter a number of customers: '))
-        while user < 0:
-            user = int(input('Please try again: '))
+#     while repeat:
+#         count = 0
+#         user = int(input('Enter a number of customers: '))
+#         while user < 0:
+#             user = int(input('Please try again: '))
 
-        # for i in range(user)
-        while count < user:
-            amount = float(input(f'Enter the amount customer {count+1} spent: '))
-            if amount < 0:
-                amount = float(input(f'Please try again and enter the amount customer{count+1} spent: '))
+#         # for i in range(user)
+#         while count < user:
+#             amount = float(input(f'Enter the amount customer {count+1} spent: '))
+#             if amount < 0:
+#                 amount = float(input(f'Please try again and enter the amount customer{count+1} spent: '))
 
-            customer_type = input(f'Enter the customer type for customer {count+1}: ').upper()
-            if customer_type != 'C' and customer_type != 'R':
-                customer_type = input(f'Please try again and enter the customer type for customer {count+1}: ').upper()
+#             customer_type = input(f'Enter the customer type for customer {count+1}: ').upper()
+#             if customer_type != 'C' and customer_type != 'R':
+#                 customer_type = input(f'Please try again and enter the customer type for customer {count+1}: ').upper()
 
-            customerDiscount = calcDiscounts(customer_type,amount)
-            subTotal = amount * customerDiscount
-            total = amount - subTotal
-            # print(f'Customer {customer_type} your subtotal was {amount}, your new total is {total} after a {customerDiscount * 100} discount')
-            print('Customer %s'%(customer_type),'your subtotal was $%.2f'%(amount),'your new total is $%.2f'%(total),'after a %.0f'%(customerDiscount*100),'% discount')
-            count += 1
+#             customerDiscount = calcDiscounts(customer_type,amount)
+#             subTotal = amount * customerDiscount
+#             total = amount - subTotal
+#             # print(f'Customer {customer_type} your subtotal was {amount}, your new total is {total} after a {customerDiscount * 100} discount')
+#             print('Customer %s'%(customer_type),'your subtotal was $%.2f'%(amount),'your new total is $%.2f'%(total),'after a %.0f'%(customerDiscount*100),'% discount')
+#             count += 1
 
-        repeat = input('Would you like to run the application again?: ').upper()
-        if repeat != 'YES':
-            break
+#         repeat = input('Would you like to run the application again?: ').upper()
+#         if repeat != 'YES':
+#             break
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
 
 ############################################################################################################
 '''
@@ -73,45 +73,46 @@ if __name__ == '__main__':
 - Next, save the values of the populations obtained from the dictionary in a list. Add to the list another list that contains the four values calculated above.
 - Practice accessing and printing out a few values in the newly created list.
 '''
-# def populations():
-#     population = {}
-#     population ['Toronto'] = 5647656
-#     population ['Ottawa'] = 1068821
-#     population ['Kitchener'] = 522888
-#     population ['London'] = 423369
-#     population ['Hamilton'] = 729560
-#     return population
+def populations():
+    population = {}
+    population ['Toronto'] = 5647656
+    population ['Ottawa'] = 1068821
+    population ['Kitchener'] = 522888
+    population ['London'] = 423369
+    population ['Hamilton'] = 729560
+    return population
 
-# def sumPop(population):
-#     totalSum = 0
-#     for value in population.values():
-#         totalSum += value
-#     return totalSum
+def sumPop(population):
+    totalSum = 0
+    for value in population.values():
+        totalSum += value
+    return totalSum
 
-# def minPop(population):
-#     return min(population.values())
+def minPop(population):
+    return min(population.values())
 
-# def maxPop(population):
-#     return max(population.values())
+def maxPop(population):
+    return max(population.values())
 
-# def avgPop(population):
-#     totalSum = 0
-#     for value in population.values():
-#         totalSum += value
-#     avg = totalSum / len(population)
-#     return avg
+def avgPop(population):
+    totalSum = 0
+    for value in population.values():
+        totalSum += value
+    avg = totalSum / len(population)
+    return avg
 
-# def main():
-#     pop = populations()
-#     newList = []
-#     newList.extend([sumPop(pop), minPop(pop), maxPop(pop), avgPop(pop)])
-#     newList2 = []
-#     newList2.append(newList)
-#     print(newList2)
-#     print(newList2[0][1]) # access the first list in the list, then access the second item in that list
+def main():
+    pop = populations()
+    newList = []
+    newList.extend([sumPop(pop), minPop(pop), maxPop(pop), avgPop(pop)])
+    newList2 = []
+    newList2.append(newList)
+    print(newList2)
+    print(newList2[0][1]) # access the first list in the list, then access the second item in that list
+    print(newList2[0][3])
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
 '''
 totalSum = sumPop(pop),minPop(pop),maxPop(pop),avgPop(pop) # this turns it into a tuple
